04733f8e8adf1c401bae0cd2bc839b428f1a7e68
Kalyanov Dmitry <kalyanowdima@gmail.ru>
2019-12-12 13:43:21 +0300

    vm can add point

--- code/polygon-area/ViewModel/src/main/java/ru/unn/agile/polygon/viewmodel/ViewModel.java
+++ code/polygon-area/ViewModel/src/main/java/ru/unn/agile/polygon/viewmodel/ViewModel.java
@@ -6,6 +6,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import ru.unn.agile.polygon.model.Polygon;
+import ru.unn.agile.polygon.model.Point;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,17 +16,68 @@ import java.util.regex.Pattern;
 public class ViewModel {
 
     private static final Pattern COORDINATE_INPUT_ALLOWED_SYMBOLS = Pattern.compile("^[-+]?[0-9]+\\.?[0-9]*$");
+    private final SimpleBooleanProperty addingNewPointDisabled = new SimpleBooleanProperty();
 
     private final StringProperty xCoordinate = new SimpleStringProperty();
     private final StringProperty yCoordinate = new SimpleStringProperty();
+    private final ObservableList<Point> pointList = FXCollections.observableArrayList();
 
     private final StringProperty result = new SimpleStringProperty();
 
+    public ViewModel() {
+        clearFormInput();
+
+        BooleanBinding canCalculateBoolBinding = new BooleanBinding() {
+            {
+                super.bind(xCoordinate, yCoordinate);
+            }
+            @Override
+            protected boolean computeValue() {
+                return (isCoordinatesInputCorrect());
+            }
+        };
+        addingNewPointDisabled.bind(canCalculateBoolBinding.not());
+    }
+
+    public boolean isCoordinatesInputCorrect() {
+        String exprText1 = xCoordinate.get();
+        String exprText2 = yCoordinate.get();
+        return (COORDINATE_INPUT_ALLOWED_SYMBOLS.matcher(exprText1).matches()
+                && COORDINATE_INPUT_ALLOWED_SYMBOLS.matcher(exprText2).matches());
+    }
+
+    public void addPoint() {
+        double x = parseCoordinate(xCoordinate);
+        double y = parseCoordinate(yCoordinate);
+        Point newPoint = new Point(x, y);
+        pointList.add(newPoint);
+
+        clearFormInput();
+    }
+
+    private void clearFormInput() {
+        xCoordinate.set("");
+        yCoordinate.set("");
+    }
+
+    private final double parseCoordinate (final StringProperty coordinate) {
+        return Double.parseDouble(coordinate.get());
+    }
 
     public StringProperty xProperty() {
         return xCoordinate;
     }
     public StringProperty yProperty() {
         return yCoordinate;
+    }
+    public ObservableList<Point> getPointList() {
+        return pointList;
+    }
+
+    public StringProperty resultProperty() {
+        return result;
+    }
+    public final String getResult() {
+        return result.get();
     }
 }
--- code/polygon-area/ViewModel/src/test/java/ru/unn/agile/polygon/viewmodel/ViewModelTests.java
+++ code/polygon-area/ViewModel/src/test/java/ru/unn/agile/polygon/viewmodel/ViewModelTests.java
@@ -3,9 +3,9 @@ package ru.unn.agile.polygon.viewmodel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ru.unn.agile.polygon.model.Point;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ViewModelTests {
     private ViewModel viewModel;
@@ -20,9 +20,16 @@ public class ViewModelTests {
         viewModel = null;
     }
 
+    @Test
+    public void isAddingValidInput () {
+        setInputData();
+        viewModel.addPoint();
+        assertEquals(viewModel.getPointList().get(0).getX(), new Point(-1, 2.5).getX(), 1.0E-10);
+        assertEquals(viewModel.getPointList().get(0).getY(), new Point(-1, 2.5).getY(), 1.0E-10);
+    }
 
     private void setInputData() {
-        viewModel.xProperty().set("1");
-        viewModel.yProperty().set("2");
+        viewModel.xProperty().set("-1");
+        viewModel.yProperty().set("2.5");
     }
 }
