0b88d4b67125a6f1334e6038672872ef211b9214
antonyvlasov332 <antonybiggestbro@gmail.com>
2019-12-15 19:29:55 +0300

    add check for negative

--- code/prime-number/Model/src/main/java/ru/unn/agile/primenumber/model/PrimeNumberFinder.java
+++ code/prime-number/Model/src/main/java/ru/unn/agile/primenumber/model/PrimeNumberFinder.java
@@ -9,6 +9,9 @@ public class PrimeNumberFinder {
     private int endSpan;
 
     public PrimeNumberFinder(final int startSpan, final int endSpan) {
+        if (startSpan < 0 || endSpan < 0) {
+            throw new IllegalArgumentException("Span's borders should be positive!");
+        }
         this.endSpan = endSpan;
         this.startSpan = startSpan;
     }
--- code/prime-number/Model/src/test/java/ru/unn/agile/primenumber/model/PrimeNumberFinderTest.java
+++ code/prime-number/Model/src/test/java/ru/unn/agile/primenumber/model/PrimeNumberFinderTest.java
@@ -13,4 +13,16 @@ public class PrimeNumberFinderTest {
         var finder = new PrimeNumberFinder(0, 10);
         assertNotNull(finder);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cannotInitFinderWithNegativeStart() {
+        var finder = new PrimeNumberFinder(-1, 10);
+        assertNotNull(finder);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cannotInitFinderWithNegativeEnd() {
+        var finder = new PrimeNumberFinder(0, -1);
+        assertNotNull(finder);
+    }
 }
