f29b4d2441465035f6b5bfbc341fbcd62a7f8289
Vladimir Tyagunov <tyagunov19@gmail.com>
2019-12-15 19:45:26 +0300

    refactor: rename fields in view main class

--- code/dijkstra-algorithm/View/src/main/java/ru/unn/agile/dijkstraalgorithm/view/Main.java
+++ code/dijkstra-algorithm/View/src/main/java/ru/unn/agile/dijkstraalgorithm/view/Main.java
@@ -9,18 +9,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private static final String APP_TITLE = "Dijkstra Algorithm";
-    private static final String APP_FXML = "Dijkstra.fxml";
+    private static final String VIEW_TITLE = "Dijkstra Algorithm";
+    private static final String FXML_RESOURCES = "Dijkstra.fxml";
     private static final String STYLE_RESOURCES = "style.css";
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(APP_FXML));
+        Parent root = FXMLLoader.load(getClass().getResource(FXML_RESOURCES));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource(STYLE_RESOURCES).toExternalForm());
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle(APP_TITLE);
+        primaryStage.setTitle(VIEW_TITLE);
         primaryStage.show();
     }
 
