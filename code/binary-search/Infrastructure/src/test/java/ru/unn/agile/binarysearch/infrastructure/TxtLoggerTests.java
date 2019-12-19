cb6c59532b0dfc24baf21f9f958984fef3cda0e8
Kirill Iuferev <juferev18@gmail.com>
2019-12-19 17:35:55 +0300

    was added more tests for txtLogger

--- code/binary-search/Infrastructure/src/test/java/ru/unn/agile/binarysearch/infrastructure/TxtLoggerTests.java
+++ code/binary-search/Infrastructure/src/test/java/ru/unn/agile/binarysearch/infrastructure/TxtLoggerTests.java
@@ -32,4 +32,39 @@ public class TxtLoggerTests {
             fail("File " + FILE_NAME + " wasn't found!");
         }
     }
+
+    @Test
+    public void DateAndTimeContainInLog() {
+        String testMessage = "Test message for check";
+
+        txtLogger.log(testMessage);
+
+        String logMessage = txtLogger.getLog().get(0);
+        assertTrue(logMessage.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} > .*"));
+    }
+
+    @Test
+    public void canWriteOneLogMessage() {
+        String testMessage = "Test message #1";
+
+        txtLogger.log(testMessage);
+
+        String logMessage = txtLogger.getLog().get(0);
+        assertTrue(logMessage.matches(".*" + logMessage + "$"));
+    }
+
+    @Test
+    public void canWriteSeveralLogMessages() {
+        String[] testMessages = {"Test message #1", "Test message #2"};
+
+        for (String testMessage : testMessages) {
+            txtLogger.log(testMessage);
+        }
+
+        List<String> logMessages = txtLogger.getLog();
+        for (int i = 0; i < logMessages.size(); i++) {
+            String logMessage = logMessages.get(i);
+            assertTrue(logMessage.matches(".*" + logMessage + "$"));
+        }
+    }
 }
